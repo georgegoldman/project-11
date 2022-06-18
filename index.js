a126676eef2b4bdeb41c96d785d08c822b8a3a6c
goldman <goldman@localhost.localdomain>
2022-06-18 21:33:33 +0100

    rm cor()

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
     config = require('./app/config/config')(),
-    cors = require('cors'),
     port = config.app.port,
     Database = require('./app/config/database'),
     walletRoute = require('./app/routes/wallets')
@@ -14,7 +13,6 @@ const
 // initialize db
 new Database(config.mongo.URI)
 
-app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
